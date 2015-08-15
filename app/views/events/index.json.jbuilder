49c766f98055cf5b1b311e479f0164ab79d4dc38
Javier Acuna Pagliero <jiacunap87@gmail.com>
2015-08-14 22:00:28 -0300

    prueba terminada

--- app/views/events/index.json.jbuilder
+++ app/views/events/index.json.jbuilder
@@ -2,5 +2,7 @@ json.array!(@events) do |event|
   json.extract! event, :id, :title
   json.start event.start_time
   json.end event.end_time
+  json.backgroundColor 'red'
+  json.backgroundColor 'blue'
   json.url event_url(event, format: :html)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814230418) do
+ActiveRecord::Schema.define(version: 20150814232057) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150814230418) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "allday"
   end
 
 end
